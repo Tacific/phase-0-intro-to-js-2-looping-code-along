c695891b6d914dad209531862dfd1d054aa29a68
Chantha Voeun <cvoeun45@gmail.com>
2023-03-06 20:09:09 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+const event = "Surprise";
+
+function writeCards(name, event){
+    let newName = [];
+    for (let i = 0; i < name.length; i++) {
+        newName.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return newName;
+}
+
+function countDown(){
+    let number = 11;
+    while (number--){
+        console.log(number);
+    }
+}
